24eeb82f85d38e7ac2428393acbc5b5161a43dbf
Chen <roochen@apac.adp.com>
2018-11-05 15:28:30 +0800

    1106-2

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-// const crossroads = require('crossroads');
 const app = express();
-
-// const hostname = 'localhost';
 const port = 3000;
 
-// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
+
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
